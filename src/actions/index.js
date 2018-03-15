c581265ce3dbbab8bc0f119222a1b620065d17ca
Thiago Abreu <thiago.abreu@gmail.com>
2018-03-15 11:10:22 -0300

    Removendo status que controla menu

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,16 @@
 export const LOAD_CATEGORY = 'LOAD_CATEGORY'
-export const SELECT_MENU = 'SELECT_MENU'
+export const LOAD_POST = 'LOAD_POST'
 
 export function loadCategory (categories) {
     return {
         type: LOAD_CATEGORY,
         categories
+    }
+}
+
+export function loadPosts (posts) {
+    return {
+        type: LOAD_POST,
+        posts
     }
 }
