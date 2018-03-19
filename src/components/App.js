06121ac14e9aec2cd0cc4bbe04a02d46786c4acd
Thiago Abreu <thiago.abreu@gmail.com>
2018-03-19 14:11:24 -0300

    Removendo coisas não utilizadas

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom'
-import { Menu, Button } from 'semantic-ui-react'
+import { Menu } from 'semantic-ui-react'
 
 import { connect } from 'react-redux'
 import { loadCategory } from '../actions'
@@ -20,9 +20,7 @@ class App extends Component {
   }
 
   render() {
-
     const { category } = this.props
-  
     return (
       <BrowserRouter>
         <div>
@@ -35,13 +33,11 @@ class App extends Component {
           )}
           <Route path="/" exact render={() => (
             <div>
-              <Button circular icon='add' color='blue' floated='right'/>
               <ListPost />
             </div>
           )}/>
           <Route path="/:category" exact render={({ match }) => (
             <div>
-              <Button circular icon='add' color='blue' floated='right'/>
               <ListPost filter={ match } />
             </div>
           )}/>
