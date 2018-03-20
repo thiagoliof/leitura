6c8c60fc3352bf7790e45b02715eae6cc89aee6b
Thiago Abreu <thiago.abreu@gmail.com>
2018-03-20 14:59:42 -0300

    Votar detalhe post

--- src/components/DetailPost.js
+++ src/components/DetailPost.js
@@ -1,21 +1,68 @@
 import React, { Component } from 'react';
+import { Button, Card, Segment, Divider } from 'semantic-ui-react'
 import { connect } from 'react-redux'
-import { fetchPost } from '../utils/api'
+import { fetchPost, votePost } from '../utils/api'
 import { loadPost } from '../actions'
 
 class DetailPost extends Component {
     
     componentDidMount() {
+        this.getPosts()
+    }
+
+    getPosts = () => {
         const { post_id } = this.props.filter.params
         const { loadPost } = this.props
         fetchPost(post_id).then(dados => {
             loadPost(dados)
         })
     }
+    
+    deletePost = (id) =>{
+        alert(`delete => ${id}`)
+        //deletePost(id).then(dados => {
+            //this.getPosts();
+        //})
+    }
+
+    voteUp = (id) => {
+        votePost(id, "upVote").then(dados => {
+            this.getPosts()
+        })
+    }
+
+    voteDown = (id) => {
+        votePost(id, "downVote").then(dados => {
+            this.getPosts()
+        })
+    }
 
     render() {
+        const { post } = this.props
+        console.log(post)
         return (
-            <div>details post</div>
+            <Segment padded>
+                <Card.Group>
+                    <Card fluid>
+                        <Card.Content>
+                            <Button circular floated='right' icon='remove' onClick={() => this.deletePost(post.id)} ></Button>
+                            <Card.Header>{ post.title }</Card.Header>
+                            <Card.Description>{ post.body }</Card.Description>
+                            <Card.Meta><Divider horizontal>Informações</Divider></Card.Meta>
+                            <Card.Meta>Autor: { post.author }</Card.Meta>
+                            <Card.Meta>Ponto(s): { post.voteScore } </Card.Meta>
+                            <Card.Meta>Comentário(s): { post.commentCount } </Card.Meta>
+                            <Card.Meta>Categoria: { post.category } </Card.Meta>
+                        </Card.Content>
+                        <Card.Content extra>
+                            <div>
+                                <Button circular icon='thumbs outline up' color='green' onClick={() => this.voteUp(post.id)}></Button>
+                                <Button circular icon='thumbs outline down' color='red' onClick={() => this.voteDown(post.id)}></Button>
+                            </div>
+                        </Card.Content>
+                    </Card>
+                </Card.Group>
+            </Segment>
         )
     }
 }
